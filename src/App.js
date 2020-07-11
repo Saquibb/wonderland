84fb6de1c773802202bc545b39a8deb3fe17751a
Usama Subhani <reachusama@protonmail.com>
2020-07-11 14:10:02 +0500

    Show alice and queen

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { useLayoutEffect, useRef } from 'react';
 // import useWebAnimations from "@wellyshen/use-web-animations";
 import './App.css';
 function App() {
-
+  
   return (
     <div className="container">
       <div className="sky"></div>
       <div className="earth">
+        <div className="alice">
+            <img className="alicesprite"  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"  alt="Alice and the Red Queen running to stay in place." />
+        </div>
       </div>>
     </div>
   );
