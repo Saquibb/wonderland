7c25abdcd0d640d99bacf4bc4b8e78a5440023bc
Usama Subhani <reachusama@protonmail.com>
2020-07-11 14:14:08 +0500

    Animate alice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { useLayoutEffect, useRef } from 'react';
-// import useWebAnimations from "@wellyshen/use-web-animations";
 import './App.css';
 function App() {
+
+  const aliceSprite = useRef(null);
+
+  useLayoutEffect(() => {
+    var spriteFrames = [
+      { transform: 'translateY(0)' },
+      { transform: 'translateY(-100%)' }   
+    ];
+
+    aliceSprite.current.animate(
+      spriteFrames, {
+        easing: 'steps(7, end)',
+        direction: "reverse",
+        duration: 500,
+        playbackRate: 1,
+        iterations: Infinity
+      });
+  })
   
   return (
     <div className="container">
       <div className="sky"></div>
       <div className="earth">
         <div className="alice">
-            <img className="alicesprite"  src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png"  alt="Alice and the Red Queen running to stay in place." />
+            <img className="alicesprite" ref={aliceSprite} src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" />
         </div>
       </div>>
     </div>
