d46ae16ff5d8675369f761eef463d5d1682169e1
Usama Subhani <reachusama@protonmail.com>
2020-07-11 15:43:23 +0500

    Add foreground tree

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import './App.css';
 function App() {
 
   const aliceSprite = useRef(null);
+  const foreground = useRef(null);
 
   useLayoutEffect(() => {
+    // Alice
       var spriteFrames = [
         { transform: 'translateY(0)' },
         { transform: 'translateY(-100%)' }   
@@ -24,7 +26,25 @@ function App() {
           alice.playbackRate -= .1;    
         } 
       }, 3000);
-    
+
+    // Scenery
+      var sceneryFrames =   [
+        { transform: 'translateX(100%)' },
+        { transform: 'translateX(-100%)' }   
+      ];
+      
+      var sceneryTimingBackground = {
+        duration: 36000,
+        iterations: Infinity
+      };
+      
+      var sceneryTimingForeground = {
+        duration: 12000,
+        iterations: Infinity
+      };
+
+      var foregroundMovement = foreground.current.animate(sceneryFrames, sceneryTimingForeground);
+
       const goFaster = () => {
         alice.playbackRate += 0.1;
       }
@@ -37,11 +57,16 @@ function App() {
   return (
     <div className="container">
       <div className="sky"></div>
+      
       <div className="earth">
         <div className="alice">
             <img className="alicesprite" ref={aliceSprite} src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/sprite_running-alice-queen_small.png" />
         </div>
       </div>>
+      
+      <div className="scenery" id="foreground1" ref={foreground}>
+        <img id="treefore" src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/641/palm3_small.png"/>
+      </div>
     </div>
   );
 }
